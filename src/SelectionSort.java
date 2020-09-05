090c5f2238854bc8b6cc2d298f0b6fcbef67430a
saurabhkhare123 <saurabh.khare_cs18@gla.ac.in>
2020-09-06 01:56:24 +0530

    1

---
+++ src/SelectionSort.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class SelectionSort {
+    public static void main(String[] args) {
+        int size,i,j ,temp;
+        Scanner sc = new Scanner(System.in);
+        size= sc.nextInt();
+        int [] a= new int[size];
+        for(i=0;i<size;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<size;i++)
+        {
+            int min= i;
+            for(j=i+1;j<size;j++)
+            {
+                if(a[j]<a[min])
+                {
+                   min=j;
+                }
+               temp=a[i];
+                a[i]=a[min];
+               a[ min]=temp;
+
+            }
+        }
+        for( i=0;i<size;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
